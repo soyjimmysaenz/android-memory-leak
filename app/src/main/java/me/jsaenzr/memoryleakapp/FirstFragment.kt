df555687fc3d643be4e7f7240db3d1c86bc1f751
Jimmy Saenz <jsaenzr@outlook.com>
2023-10-07 21:25:54 -0600

    Initial commit

---
+++ app/src/main/java/me/jsaenzr/memoryleakapp/FirstFragment.kt
@@ -0,0 +1,40 @@
+package me.jsaenzr.memoryleakapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import me.jsaenzr.memoryleakapp.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupWith(binding)
+    }
+
+    private fun setupWith(binding: FragmentFirstBinding) = with(binding) {
+        buttonNavigate.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
